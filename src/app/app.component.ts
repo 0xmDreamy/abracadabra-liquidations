30f68c37d81c1a2e4a74ee8e5068068ce24a8c90
0xmDreamy <0xmDreamy@proton.me>
2023-02-07 21:04:39 +0100

    Remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Liquidation, LiquidationsService } from './liquidations.service';
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -8,24 +6,5 @@ import { Liquidation, LiquidationsService } from './liquidations.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
-  liquidations: Liquidation[] | null = null
-  liquidationsSubscription: Subscription | null = null
-
-  constructor(private liquidationService: LiquidationsService) {}
-
-  onNewAddress(address: string | null): void {
-    this.liquidationsSubscription?.unsubscribe()
-    if (address != null) {
-      this.liquidationService.getLiquidations(address).subscribe((liquidations) => {
-        this.liquidations = liquidations
-      })
-    } else {
-      this.liquidations = null
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.liquidationsSubscription?.unsubscribe()
-  }
+export class AppComponent {
 }
