39aa6bb102879c05424bf940844e2872488b503e
0xmDreamy <0xmDreamy@proton.me>
2023-02-18 15:31:12 +0100

    Remove all tests

--- src/app/explorer.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ExplorerPipe } from './explorer.pipe'
-
-describe('ExplorerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ExplorerPipe()
-    expect(pipe).toBeTruthy()
-  })
-})
